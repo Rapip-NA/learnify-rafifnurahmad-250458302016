8791474cf26e3fdb84c39b5d65e3e632c59fce40
rafif nur ahmad <rapipna19@gmail.com>
2025-11-27 19:38:49 +0700

    blom selesai

--- resources/views/components/layouts/partials/sidebar.blade.php
+++ resources/views/components/layouts/partials/sidebar.blade.php
@@ -20,10 +20,9 @@
 
         <div class="sidebar-menu">
             @if (Auth::user()->role === 'admin')
-
                 <ul class="menu">
                     <li class="sidebar-title">Menu</li>
-                    <li class="sidebar-item">
+                    <li class="sidebar-item {{ request()->routeIs('admin.dashboard') ? 'active' : '' }}">
                         <a href="{{ route('admin.dashboard') }}" class="sidebar-link">
                             <i class="bi bi-grid-fill"></i>
                             <span>Dashboard</span>
@@ -31,28 +30,28 @@
                     </li>
 
                     <li class="sidebar-title">Main Menu</li>
-                    <li class="sidebar-item">
+                    <li class="sidebar-item {{ request()->routeIs('admin.competitions*') ? 'active' : '' }}">
                         <a href="{{ route('admin.competitions.index') }}" class="sidebar-link">
                             <i class="bi bi-trophy"></i>
                             <span>Competitions</span>
                         </a>
                     </li>
 
-                    <li class="sidebar-item">
+                    <li class="sidebar-item {{ request()->routeIs('admin.categories*') ? 'active' : '' }}">
                         <a href="{{ route('admin.categories.index') }}" class="sidebar-link">
                             <i class="bi bi-tags"></i>
                             <span>Category</span>
                         </a>
                     </li>
 
-                    <li class="sidebar-item">
+                    <li class="sidebar-item {{ request()->routeIs('admin.questions*') ? 'active' : '' }}">
                         <a href="{{ route('admin.questions.index') }}" class="sidebar-link">
                             <i class="bi bi-card-checklist"></i>
                             <span>Question</span>
                         </a>
                     </li>
 
-                    <li class="sidebar-item">
+                    <li class="sidebar-item {{ request()->routeIs('admin.badges*') ? 'active' : '' }}">
                         <a href="{{ route('admin.badges.index') }}" class="sidebar-link">
                             <i class="bi bi-award"></i>
                             <span>Badges</span>
@@ -60,14 +59,14 @@
                     </li>
 
                     <li class="sidebar-title">List Anggota</li>
-                    <li class="sidebar-item">
+                    <li class="sidebar-item {{ request()->routeIs('admin.peserta*') ? 'active' : '' }}">
                         <a href="{{ route('admin.peserta.index') }}" class="sidebar-link">
                             <i class="bi bi-people-fill"></i>
                             <span>Peserta</span>
                         </a>
                     </li>
 
-                    <li class="sidebar-item">
+                    <li class="sidebar-item {{ request()->routeIs('admin.qualifier*') ? 'active' : '' }}">
                         <a href="{{ route('admin.qualifier.index') }}" class="sidebar-link">
                             <i class="bi bi-people-fill"></i>
                             <span>Qualifier</span>
@@ -75,78 +74,73 @@
                     </li>
 
                     <li class="sidebar-title">Pages</li>
-                    <li class="sidebar-item">
+                    <li class="sidebar-item {{ request()->routeIs('global.leaderboard') ? 'active' : '' }}">
                         <a href="{{ route('global.leaderboard') }}" class="sidebar-link">
                             <i class="bi bi-bar-chart-fill"></i>
                             <span>Leaderboard</span>
                         </a>
                     </li>
 
-                    <li class="sidebar-item">
+                    <li class="sidebar-item {{ request()->routeIs('admin.analytics') ? 'active' : '' }}">
                         <a href="{{ route('admin.analytics') }}" class="sidebar-link">
                             <i class="bi bi-graph-up"></i>
                             <span>Analytics</span>
                         </a>
                     </li>
                 </ul>
-
             @elseif (Auth::user()->role === 'peserta')
-
                 <ul class="menu">
-                    <li class="sidebar-item">
+                    <li class="sidebar-item {{ request()->routeIs('peserta.dashboard') ? 'active' : '' }}">
                         <a href="{{ route('peserta.dashboard') }}" class="sidebar-link">
                             <i class="bi bi-grid-fill"></i>
                             <span>Dashboard</span>
                         </a>
                     </li>
 
-                    <li class="sidebar-item">
+                    <li class="sidebar-item {{ request()->routeIs('peserta.competitions*') ? 'active' : '' }}">
                         <a href="{{ route('peserta.competitions.list') }}" class="sidebar-link">
                             <i class="bi bi-trophy-fill"></i>
                             <span>Competitions</span>
                         </a>
                     </li>
 
-                    <li class="sidebar-item">
+                    <li class="sidebar-item {{ request()->routeIs('peserta.my-badges') ? 'active' : '' }}">
                         <a href="{{ route('peserta.my-badges') }}" class="sidebar-link">
                             <i class="bi bi-award-fill"></i>
                             <span>My Badges</span>
                         </a>
                     </li>
 
-                    <li class="sidebar-item">
+                    <li class="sidebar-item {{ request()->routeIs('global.leaderboard') ? 'active' : '' }}">
                         <a href="{{ route('global.leaderboard') }}" class="sidebar-link">
                             <i class="bi bi-bar-chart-fill"></i>
                             <span>Leaderboard</span>
                         </a>
                     </li>
                 </ul>
-
             @else
-
                 <ul class="menu">
-                    <li class="sidebar-item">
+                    <li class="sidebar-item {{ request()->routeIs('qualifier.dashboard') ? 'active' : '' }}">
                         <a href="{{ route('qualifier.dashboard') }}" class="sidebar-link">
                             <i class="bi bi-grid-fill"></i>
                             <span>Dashboard</span>
                         </a>
                     </li>
 
-                    <li class="sidebar-item">
+                    <li class="sidebar-item {{ request()->routeIs('qualifier.answer-validation') ? 'active' : '' }}">
                         <a href="{{ route('qualifier.answer-validation') }}" class="sidebar-link">
                             <i class="bi bi-check2-square"></i>
                             <span>Answer Validation</span>
                         </a>
                     </li>
 
-                    <li class="sidebar-item">
+                    <li class="sidebar-item {{ request()->routeIs('global.leaderboard') ? 'active' : '' }}">
                         <a href="{{ route('global.leaderboard') }}" class="sidebar-link">
                             <i class="bi bi-bar-chart-fill"></i>
                             <span>Leaderboard</span>
                         </a>
                     </li>
                 </ul>
-
             @endif
         </div>
     </div>
---
+++ resources/views/errors/500.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>500 - System Error</title>
+    <link rel="shortcut icon" href="{{ asset('mazer/assets/compiled/svg/favicon.svg') }}" type="image/x-icon">
+    <link rel="stylesheet" href="{{ asset('mazer/assets/compiled/css/app.css') }}">
+    <link rel="stylesheet" href="{{ asset('mazer/assets/compiled/css/error.css') }}">
+</head>
+
+<body>
+    <script src="{{ asset('mazer/assets/static/js/initTheme.js') }}"></script>
+    <div id="error">
+
+        <div class="error-page container">
+            <div class="col-md-8 col-12 offset-md-2">
+                <div class="text-center">
+                    <img class="img-error" src="{{ asset('mazer/assets/compiled/svg/error-500.svg') }}"
+                        alt="System Error">
+                    <h1 class="error-title">System Error</h1>
+                    <p class="fs-5 text-gray-600">The website is currently unavailable. Try again later or contact the
+                        developer.</p>
+                    <a href="{{ url('/') }}" class="btn btn-lg btn-outline-primary mt-3">Go Home</a>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</body>
+
+</html>
